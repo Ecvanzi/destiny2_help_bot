543878edfd7b51a7e39074edeb922854165b655d
Lev <ecvanzi@gmail.com>
2019-12-16 14:48:22 +0300

    коты

--- handlers.py
+++ handlers.py
@@ -2,5 +2,7 @@
 
 
 def greet_user(bot, update, user_data):
-    text = 'Привет {}'.format(update.message.chat.first_name)
+    text = '''
+    Привет {}. Я бот-помощник по игре Destiny 2.
+    У меня ты можешь узнать какие испытания актуальны на этой неделе, "где Ксур?", какая сейчас неделя проклятья и как пройти рейд.'''.format(update.message.chat.first_name)
     update.message.reply_text(text)

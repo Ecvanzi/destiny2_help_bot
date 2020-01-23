1896fcbb948602fe5e83e8bb27a59aa1e15f8d74
Lev <ecvanzi@gmail.com>
2020-01-23 19:11:16 +0300

    db

--- events.py
+++ events.py
@@ -4,119 +4,86 @@ from events_keyboards import *
 
 
 def glimmer_extraction(update, context):
-    query = update.callback_query
     page = 'first_page'
-    event = db.open_events.find_one({"event_name":"Добыча блеска."}, {"event_name": 1, "about":1, "heroic":1, "_id":0})
-    context.bot.send_message(chat_id=query.message.chat.id, text='{}'.format(event['event_name']))
-    context.bot.send_message(chat_id=query.message.chat.id, text='Обычное прохождение: {}'.format(event['about']))
-    context.bot.send_message(chat_id=query.message.chat.id, text='Героический режим: {}'.format(event['heroic']))
+    event_name = "Добыча блеска."
+    send_event(update, context, event_name)
     key_menu(update, context, page)
     
 def weapon_trade(update, context):
-    query = update.callback_query
     page = 'first_page'
-    event = db.open_events.find_one({"event_name":"Обмен оружием."}, {"event_name": 1, "about":1, "heroic":1, "_id":0})
-    context.bot.send_message(chat_id=query.message.chat.id, text='{}'.format(event['event_name']))
-    context.bot.send_message(chat_id=query.message.chat.id, text='Обычное прохождение: {}'.format(event['about']))
-    context.bot.send_message(chat_id=query.message.chat.id, text='Героический режим: {}'.format(event['heroic']))
+    event_name = "Обмен оружием."
+    send_event(update, context, event_name)
     key_menu(update, context, page)
 
 def ether_resupply(update, context):
-    query = update.callback_query
     page = 'first_page'
-    event = db.open_events.find_one({"event_name":"Пополнение запасов эфира."}, {"event_name": 1, "about":1, "heroic":1, "_id":0})
-    context.bot.send_message(chat_id=query.message.chat.id, text='{}'.format(event['event_name']))
-    context.bot.send_message(chat_id=query.message.chat.id, text='Обычное прохождение: {}'.format(event['about']))
-    context.bot.send_message(chat_id=query.message.chat.id, text='Героический режим: {}'.format(event['heroic']))
+    event_name = "Пополнение запасов эфира."
+    send_event(update, context, event_name)
     key_menu(update, context, page)
 
 def cabal_excavation(update, context):
-    query = update.callback_query
     page = 'second_page'
-    event = db.open_events.find_one({"event_name":"Раскопки Кабал."}, {"event_name": 1, "about":1, "heroic":1, "_id":0})
-    context.bot.send_message(chat_id=query.message.chat.id, text='{}'.format(event['event_name']))
-    context.bot.send_message(chat_id=query.message.chat.id, text='Обычное прохождение: {}'.format(event['about']))
-    context.bot.send_message(chat_id=query.message.chat.id, text='Героический режим: {}'.format(event['heroic']))
+    event_name = "Раскопки Кабал."
+    send_event(update, context, event_name)
     key_menu(update, context, page)
     
 def taken_blight(update, context):
-    query = update.callback_query
     page = 'second_page'
-    event = db.open_events.find_one({"event_name":"Очаг одержимых."}, {"event_name": 1, "about":1, "heroic":1, "_id":0})
-    context.bot.send_message(chat_id=query.message.chat.id, text='{}'.format(event['event_name']))
-    context.bot.send_message(chat_id=query.message.chat.id, text='Обычное прохождение: {}'.format(event['about']))
-    context.bot.send_message(chat_id=query.message.chat.id, text='Героический режим: {}'.format(event['heroic']))
+    event_name = "Очаг одержимых."
+    send_event(update, context, event_name)
     key_menu(update, context, page)
 
 def injection_rig(update, context):
-    query = update.callback_query
     page = 'second_page'
-    event = db.open_events.find_one({"event_name":"Впускной механизм."}, {"event_name": 1, "about":1, "heroic":1, "_id":0})
-    context.bot.send_message(chat_id=query.message.chat.id, text='{}'.format(event['event_name']))
-    context.bot.send_message(chat_id=query.message.chat.id, text='Обычное прохождение: {}'.format(event['about']))
-    context.bot.send_message(chat_id=query.message.chat.id, text='Героический режим: {}'.format(event['heroic']))
+    event_name = "Впускной механизм."
+    send_event(update, context, event_name)
     key_menu(update, context, page)
 
 def witches_ritual(update, context):
-    query = update.callback_query
     page = 'third_page'
-    event = db.open_events.find_one({"event_name":"Ведьминский ритуал."}, {"event_name": 1, "about":1, "heroic":1, "_id":0})
-    context.bot.send_message(chat_id=query.message.chat.id, text='{}'.format(event['event_name']))
-    context.bot.send_message(chat_id=query.message.chat.id, text='Обычное прохождение: {}'.format(event['about']))
-    context.bot.send_message(chat_id=query.message.chat.id, text='Героический режим: {}'.format(event['heroic']))
+    event_name = "Ведьминский ритуал."
+    send_event(update, context, event_name)
     key_menu(update, context, page)
 
 def spire_integration(update, context):
-    query = update.callback_query
     page = 'third_page'
-    event = db.open_events.find_one({"event_name":"Интеграция шпиля."}, {"event_name": 1, "about":1, "heroic":1, "_id":0})
-    context.bot.send_message(chat_id=query.message.chat.id, text='{}'.format(event['event_name']))
-    context.bot.send_message(chat_id=query.message.chat.id, text='Обычное прохождение: {}'.format(event['about']))
-    context.bot.send_message(chat_id=query.message.chat.id, text='Героический режим: {}'.format(event['heroic']))
+    event_name = "Интеграция шпиля."
+    send_event(update, context, event_name)
     key_menu(update, context, page)
 
 def fallen_satellite(update, context):
-    query = update.callback_query
     page = 'third_page'
-    event = db.open_events.find_one({"event_name":"Упавший спутник."}, {"event_name": 1, "about":1, "heroic":1, "_id":0})
-    context.bot.send_message(chat_id=query.message.chat.id, text='{}'.format(event['event_name']))
-    context.bot.send_message(chat_id=query.message.chat.id, text='Обычное прохождение: {}'.format(event['about']))
-    context.bot.send_message(chat_id=query.message.chat.id, text='Героический режим: {}'.format(event['heroic']))
+    event_name = "Упавший спутник."
+    send_event(update, context, event_name)
     key_menu(update, context, page)
 
 def vex_gate_lord(update, context):
-    query = update.callback_query
     page = 'fourth_page'
-    event = db.open_events.find_one({"event_name":"Перекрестье вексов."}, {"event_name": 1, "about":1, "heroic":1, "_id":0})
-    context.bot.send_message(chat_id=query.message.chat.id, text='{}'.format(event['event_name']))
-    context.bot.send_message(chat_id=query.message.chat.id, text='Обычное прохождение: {}'.format(event['about']))
-    context.bot.send_message(chat_id=query.message.chat.id, text='Героический режим: {}'.format(event['heroic']))
+    event_name = "Перекрестье вексов."
+    send_event(update, context, event_name)
     key_menu(update, context, page)
 
 def cryocapsule_escape(update, context):
-    query = update.callback_query
     page = 'fourth_page'
-    event = db.open_events.find_one({"event_name":"Побег в крио-капсуле."}, {"event_name": 1, "about":1, "heroic":1, "_id":0})
-    context.bot.send_message(chat_id=query.message.chat.id, text='{}'.format(event['event_name']))
-    context.bot.send_message(chat_id=query.message.chat.id, text='Обычное прохождение: {}'.format(event['about']))
-    context.bot.send_message(chat_id=query.message.chat.id, text='Героический режим: {}'.format(event['heroic']))
+    event_name = "Побег в крио-капсуле."
+    send_event(update, context, event_name)
     key_menu(update, context, page)
 
 def ether_ritual(update, context):
-    query = update.callback_query
     page = 'fourth_page'
-    event = db.open_events.find_one({"event_name":"Эфирный ритуал."}, {"event_name": 1, "about":1, "heroic":1, "_id":0})
-    context.bot.send_message(chat_id=query.message.chat.id, text='{}'.format(event['event_name']))
-    context.bot.send_message(chat_id=query.message.chat.id, text='Обычное прохождение: {}'.format(event['about']))
-    context.bot.send_message(chat_id=query.message.chat.id, text='Героический режим: {}'.format(event['heroic']))
+    event_name = "Эфирный ритуал."
+    send_event(update, context, event_name)
     key_menu(update, context, page)
 
 def rift_generator(update, context):
-    query = update.callback_query
     page = 'fifth_page'
-    event = db.open_events.find_one({"event_name":"Генератор разлома."}, {"event_name": 1, "about":1, "heroic":1, "_id":0})
+    event_name = "Генератор разлома."
+    send_event(update, context, event_name)
+    key_menu(update, context, page)
+
+def send_event(update, context, event_name):
+    query = update.callback_query
+    event = db.open_events.find_one({"event_name": event_name }, {"event_name": 1, "about":1, "heroic":1, "_id":0})
     context.bot.send_message(chat_id=query.message.chat.id, text='{}'.format(event['event_name']))
     context.bot.send_message(chat_id=query.message.chat.id, text='Обычное прохождение: {}'.format(event['about']))
     context.bot.send_message(chat_id=query.message.chat.id, text='Героический режим: {}'.format(event['heroic']))
-    key_menu(update, context, page)
-
